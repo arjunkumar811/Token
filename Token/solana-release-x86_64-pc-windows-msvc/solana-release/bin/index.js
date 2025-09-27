5da052180173422352dac19cda1872b6959951d0
arjunkumar811 <kumararjun26401@gmail.com>
2025-09-27 22:37:57 +0530

    Added Token logic

--- Token/solana-release-x86_64-pc-windows-msvc/solana-release/bin/index.js
+++ Token/solana-release-x86_64-pc-windows-msvc/solana-release/bin/index.js
@@ -1,13 +1,100 @@
-
 const {Connection, LAMPORTS_PER_SOL, clusterApiUrl, PublicKey} = require('@solana/web3.js');
 
-const connection = new Connection(clusterApiUrl('devnet'));
+const connection = new Connection("https://solana-devnet.g.alchemy.com/v2/FffcU9C6hYnq9sIaWyD2ByCpIQAI07z_", "confirmed");
 
 async function airdrop(publicKey, amount) {
     const airdropSignature = await connection.requestAirdrop(new PublicKey(publicKey), amount);
     await connection.confirmTransaction({signature: airdropSignature})
 }
 
-airdrop("GokppTzVZi2LT1MSTWoEprM4YLDPy7wQ478Rm3r77yEw", LAMPORTS_PER_SOL).then(signature => {
+airdrop("CBXK8LgoSfpqjp9EGqSDeqnqBdoq5dSbMiaSJUCgW1AA", LAMPORTS_PER_SOL).then(signature => {
     console.log('Airdrop signature:', signature);
 });
+
+
+
+
+// Create token mint
+
+const { createMint } = require('@solana/spl-token');
+const { Keypair, Connection, clusterApiUrl,  TOKEN_PROGRAM_ID } = require('@solana/web3.js');
+
+const payer = Keypair.fromSecretKey(Uint8Array.from([102,144,169,42,220,87,99,85,100,128,197,17,41,234,250,84,87,98,161,74,15,249,83,6,120,159,135,22,46,164,204,141,234,217,146,214,61,187,254,97,124,111,61,29,54,110,245,186,11,253,11,127,213,20,73,8,25,201,22,107,4,75,26,120]));
+
+const mintAthority = payer;
+
+const connection = new Connection(clusterApiUrl('devnet'));
+
+async function createMintForToken(payer, mintAuthority) {
+    const mint = await createMint(
+        connection,
+        payer,
+        mintAuthority,
+        null,
+        6,
+        TOKEN_PROGRAM_ID
+    );
+    console.log('Mint created at', mint.toBase58());
+    return mint;
+}
+
+async function main() {
+    const mint = await createMintForToken(payer, mintAthority.publicKey);
+}
+
+main();
+
+
+
+
+
+
+// Create an associated token account, mint some tokens
+const { createMint, getOrCreateAssociatedTokenAccount, mintTo } = require('@solana/spl-token');
+const { Keypair, Connection, clusterApiUrl,  TOKEN_PROGRAM_ID, PublicKey } = require('@solana/web3.js');
+
+const payer = Keypair.fromSecretKey(Uint8Array.from([102,144,169,42,220,87,99,85,100,128,197,17,41,234,250,84,87,98,161,74,15,249,83,6,120,159,135,22,46,164,204,141,234,217,146,214,61,187,254,97,124,111,61,29,54,110,245,186,11,253,11,127,213,20,73,8,25,201,22,107,4,75,26,120]));
+
+const mintAthority = payer;
+
+const connection = new Connection(clusterApiUrl('devnet'));
+
+async function createMintForToken(payer, mintAuthority) {
+    const mint = await createMint(
+        connection,
+        payer,
+        mintAuthority,
+        null,
+        6,
+        TOKEN_PROGRAM_ID
+    );
+    console.log('Mint created at', mint.toBase58());
+    return mint;
+}
+
+async function mintNewTokens(mint, to, amount) { 
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        payer,
+        mint,
+        new PublicKey(to)
+      );
+
+      console.log('Token account created at', tokenAccount.address.toBase58());
+      await mintTo(
+        connection,
+        payer,
+        mint,
+        tokenAccount.address,
+        payer,
+        amount
+      )
+      console.log('Minted', amount, 'tokens to', tokenAccount.address.toBase58());
+}
+
+async function main() {
+    const mint = await createMintForToken(payer, mintAthority.publicKey);
+    await mintNewTokens(mint, mintAthority.publicKey, 100);    
+}
+
+main();
